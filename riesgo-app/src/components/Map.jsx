8c70e863e402cbc7ee2fb2d24150dd117aece920
Your Name <jacob_darvin@dlsu.edu.ph>
2021-12-30 14:22:44 +0800

    added 3d button

--- riesgo-app/src/components/Map.jsx
+++ riesgo-app/src/components/Map.jsx
@@ -49,8 +49,54 @@ export default class Map extends Component {
           bearing: 0.13
         });
 
+        //Buttons
+        class PitchToggle {
+            constructor({ bearing = 0, pitch = 45, minpitchzoom = null }) {
+              this._bearing = bearing;
+              this._pitch = pitch;
+              this._minpitchzoom = minpitchzoom;
+            }
+          
+            onAdd(map) {
+              this._map = map;
+              let _this = this;
+          
+              this._btn = document.createElement("button");
+              this._btn.className = "mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-3d";
+              this._btn.type = "button";
+              this._btn["aria-label"] = "Toggle Pitch";
+              this._btn.onclick = function() {
+                if (map.getPitch() === 0) {
+                  let options = { pitch: _this._pitch, bearing: _this._bearing };
+                  if (_this._minpitchzoom && map.getZoom() > _this._minpitchzoom) {
+                    options.zoom = _this._minpitchzoom;
+                  }
+                  map.easeTo(options);
+                  _this._btn.className =
+                    "mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-2d";
+                } else {
+                  map.easeTo({ pitch: 0, bearing: 0 });
+                  _this._btn.className =
+                    "mapboxgl-ctrl-icon mapboxgl-ctrl-pitchtoggle-3d";
+                }
+              };
+          
+              this._container = document.createElement("div");
+              this._container.className = "mapboxgl-ctrl-group mapboxgl-ctrl";
+              this._container.appendChild(this._btn);
+          
+              return this._container;
+            }
+          
+            onRemove() {
+              this._container.parentNode.removeChild(this._container);
+              this._map = undefined;
+            }
+        }
+
+        this.map.addControl(new PitchToggle({ minpitchzoom: 11 }), "top-right");
         this.map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-        
+
         this.map.on('style.load', () => {
             //MARIKINA ==================================================================================
             //Marikina Dataset - Land Elevation
