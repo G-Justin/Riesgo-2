413d58b623501915a07308e206816d56ceb97e76
Jacob Darvin <jacob_darvin@dlsu.edu.ph>
2022-01-25 17:03:58 +0800

    Update Analysis.jsx

--- riesgo-app/src/components/Analysis.jsx
+++ riesgo-app/src/components/Analysis.jsx
@@ -1,55 +1,127 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
-import { BarChart, Bar, Tooltip, Legend, XAxis, Cell } from "recharts";
+import { 
+    BarChart, 
+    Bar, 
+    Tooltip, 
+    XAxis, 
+    Cell,
+} from "recharts";
 
-// const green = "#58C0A1"
-// const teal = "#53C2E2"
-// const blue = "#5597DE"
-// const orange = "#F2B35B"
-// const red = "#F16D64"
-    
-const getColor = (p) => {
-        // if (p >= 90) { return green; }
-        // if (p < 90 && p >= 80) { return teal; }
-        // if (p < 80 && p >= 70) { return blue; }
-        // if (p < 70 && p >= 60) { return orange; }
-        // if (p < 60) { return red; }
-        if(p === 1) {return "#cfd3ff"}
-        if(p === 0.66) {return "#727ded"}
-        if(p === 0.33) {return "#3741a1"}
-        if(p === 0) {return "#06106b"}
+const getFloodColor = (p) => {
+        if (p >= 0.9) { return "#cfd3ff"; }
+        if (p < .90 && p >= .60) { return "#727ded"; }
+        if (p < .60 && p >= .30) { return "#3741a1"; }
+        if (p < .30 && p >= 0.10) { return "#06106b"; }
+        if (p < .10) { return "#06106b"; }
 }
 
-const data = [
+const getHazardColor = (p) => {
+    if (p >= 0.9) { return "#ffd86e"; }
+    if (p < .90 && p >= .60) { return "#ffd86e"; }
+    if (p < .60 && p >= .30) { return "#f6684c"; }
+    if (p < .30 && p >= 0.10) { return "#9d1e69"; }
+    if (p < .10) { return "#300061"; }
+}
+
+const getAccessibilityColor = (p) => {
+    if (p >= 0.66) { return "#800f2f"; }
+    if (p < .66 && p >= .33) { return "#c9184a"; }
+    if (p >= 0) { return "#ffc2ce"; }
+}
+
+const floodData = [
   {
     name: "5 Years",
-    Flood_Score: 0.33,
+    a: 1,
   },
   {
     name: "25 Years",
-    Flood_Score: 0.66,
+    a: 0.66,
   },
   {
     name: "100 Years",
-    Flood_Score: 1,
+    a: 0.33,
   },
+];
+
+const hazardData = [
+    {
+      name: "5 Years",
+      a: 0.75,
+    },
+    {
+      name: "25 Years",
+      a: 0.5,
+    },
+    {
+      name: "100 Years",
+      a: 0.25,
+    },
+  ];
+
+const accessibilityData = [
+    {
+      name: "5 Years",
+      a: 0.75,
+    },
+    {
+      name: "25 Years",
+      a: 0.5,
+    },
+    {
+      name: "100 Years",
+      a: 0.25,
+    },
 ];
 
 export default function Analysis() {
   return (
     <div>
-    <Typography variant="h6">Flood</Typography>
+    <i>Higher is better*</i>
+    <Typography variant="h6">Flood Safety Score</Typography>
     <Box 
         display="flex" 
         alignItems="center"
         justifyContent="center">
-        <BarChart width={320} height={150} data={data}>
+        <BarChart width={320} height={150} data={floodData}>
             <XAxis dataKey="name" />
             <Tooltip />
-            <Legend />
-            <Bar dataKey="Flood_Score">
-            {data.map((entry, index) => (
-                <Cell fill={getColor(entry.Flood_Score)} />
+            <Bar dataKey="a">
+            {floodData.map((entry, index) => (
+                <Cell fill={getFloodColor(entry.a)} />
+            ))}
+            </Bar>
+        </BarChart>
+    </Box>
+
+    <Typography variant="h6">Hazard Safety Score</Typography>
+    <Box 
+        display="flex" 
+        alignItems="center"
+        justifyContent="center">
+        <BarChart width={320} height={150} data={hazardData}>
+            <XAxis dataKey="name" />
+            <Tooltip />
+            <Bar dataKey="a">
+            {hazardData.map((entry, index) => (
+                <Cell fill={getHazardColor(entry.a)} />
+            ))}
+            </Bar>
+        </BarChart>
+    </Box>
+
+    <Typography variant="h6">Area Accessibility Score</Typography>
+    <Box 
+        display="flex" 
+        alignItems="center"
+        justifyContent="center">
+        <BarChart width={320} height={150} data={accessibilityData}>
+            <XAxis dataKey="name" />
+            <Tooltip />
+            <Bar dataKey="a">
+            {accessibilityData.map((entry, index) => (
+                <Cell fill={getAccessibilityColor(entry.a)} />
             ))}
             </Bar>
         </BarChart>
