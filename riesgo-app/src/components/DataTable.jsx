0d2406096021e10769c97bac725d599d39aa7e3b
Jacob Darvin <jacob_darvin@dlsu.edu.ph>
2022-02-06 22:24:15 +0800

    Update DataTable.jsx

--- riesgo-app/src/components/DataTable.jsx
+++ riesgo-app/src/components/DataTable.jsx
@@ -3,7 +3,6 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
@@ -41,12 +40,6 @@ function createData(name, calories, fat, carbs, protein) {
     return (
       <TableContainer component={Paper}>
         <Table aria-label="caption table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Properties</TableCell>
-              <TableCell align="right">Values</TableCell>
-            </TableRow>
-          </TableHead>
           <TableBody>
             {rows.map((row) => (
               <TableRow key={row.name}>
