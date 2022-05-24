1708bec0fc33ec74184dac43714f19fed3e48fd6
Jacob Darvin <jacob_darvin@dlsu.edu.ph>
2022-05-24 13:42:17 +0800

    welcome page changes

--- riesgo-app/src/components/Welcome.jsx
+++ riesgo-app/src/components/Welcome.jsx
@@ -9,31 +9,32 @@ function Welcome() {
     return (
         <div>
             <section className="body-section" >
-                <div className="container-body">
-                    <div className="content-body">
-                        <div className="left-side">
-                            <LogoSvg />
-                            <Typography variant='h1'>
-                                An Evacuation Center Suitability Tool.
-                            </Typography>
-                            <Typography variant='body2'>
-                                We're working on building an <b>interactive Metro Manila Map for flood visualization and urban planning</b>.
-                                See our paper about it <a href="#">here</a>.
-                            </Typography>
-                            <br />
-                            <div>
-                                <Link to="map" style={{ color: "white", textDecoration: "none" }}>
-                                    <button className="btn-grad">
-                                        BEGIN RIESGO
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="right-side">
-                            <img src='right2.png' alt='' style={{width:'40em'}} />
+                <div className="new-content-body">
+                    <div className='left-content'>
+                        <LogoSvg />
+                        <Typography variant='h2'>
+                            An Evacuation Center Suitability Tool.
+                        </Typography>
+                        <Typography variant='body2'>
+                            We're working on building an <b>interactive Metro Manila Map for flood visualization and urban planning</b>.
+                            See our paper about it <a href="https://comet.dlsu.edu.ph" target="_blank" rel="noreferrer">here</a>.
+                        </Typography>
+                        
+                        <div style={{marginTop: '0.5em'}}>
+                            <Link to="map" style={{ color: "white", textDecoration: "none" }}>
+                                <button className="btn-grad">
+                                    BEGIN RIESGO
+                                </button>
+                            </Link>
                         </div>
                     </div>
+
+                    <div className='right-graphic'>
+                        <img src='right2.png' alt='' className='right-image'/>
+                    </div>
+
                 </div>
+
                 <footer>
                     <div className="footer">
                         <div>
